f9f38e9f1f0f68ea30a1dd194838d956926a86b3
Trent Buckholz <trent.buckholz@c3.ai>
2025-11-15 09:45:28 -0800

    feat!: Add support for db migrations using goose.
    Added migration for users table.

--- cmd/main.go
+++ cmd/main.go
@@ -1,14 +1,18 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"log"
 
 	"github.com/gin-gonic/gin"
+	_ "github.com/jackc/pgx/v5/stdlib"
+	"github.com/pressly/goose/v3"
 	"github.com/spf13/viper"
 )
 
 func main() {
+	// Read env config.
 	viper.SetConfigName("env")
 	viper.SetConfigType("toml")
 	viper.AddConfigPath(".")
@@ -16,15 +20,29 @@ func main() {
 	if err := viper.ReadInConfig(); err != nil {
 		panic(fmt.Errorf("fatal error config file: %w", err))
 	}
-	port := viper.GetInt("server.port")
+	serverPort := viper.GetInt("server.port")
+	dbUser := viper.GetString("database.user")
+	dbPort := viper.GetInt("database.port")
+	dbName := viper.GetString("database.name")
+	dbHost := viper.GetString("database.host")
 
+	db, err := sql.Open("pgx", fmt.Sprintf("host=%s user=%s port=%d dbname=%s sslmode=disable", dbHost, dbUser, dbPort, dbName))
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer func() { _ = db.Close() }()
+	if err := goose.Up(db, "migrations"); err != nil {
+		log.Fatal(err)
+	}
+
+	// Start server.
 	r := gin.Default()
 	r.GET("/", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "Hello, Gin!",
 		})
 	})
-	if err := r.Run(fmt.Sprintf(":%d", port)); err != nil {
+	if err := r.Run(fmt.Sprintf(":%d", serverPort)); err != nil {
 		log.Fatal(err)
 	}
 }
---
+++ migrations/20251115095049_add_user_table.sql
@@ -0,0 +1,14 @@
+-- +goose Up
+CREATE TABLE users (
+    id BIGSERIAL PRIMARY KEY,
+    email TEXT UNIQUE,
+    username TEXT UNIQUE,
+    pw_hash TEXT,
+    salt TEXT,
+    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMP NOT NULL DEFAULT NOW()
+
+);
+
+-- +goose Down
+DROP TABLE users;
