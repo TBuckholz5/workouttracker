bf78a59ce38f02de6fbb959762e5375a34f5de85
Trent Buckholz <trent.buckholz@c3.ai>
2025-11-18 14:43:27 -0800

    feat: Added migration for exercises table.

--- migrations/20251118144304_add_exercises_table.sql
+++ migrations/20251118144304_add_exercises_table.sql
@@ -1,12 +1,13 @@
 -- +goose Up
-CREATE TABLE users (
+CREATE TABLE exercises (
     id BIGSERIAL PRIMARY KEY,
-    email TEXT UNIQUE,
-    username TEXT UNIQUE,
-    pw_hash TEXT,
+    name text NOT NULL,
+    description TEXT,
+    target_muscle TEXT,
+    picture_url TEXT,
     created_at TIMESTAMP NOT NULL DEFAULT NOW(),
     updated_at TIMESTAMP NOT NULL DEFAULT NOW()
 );
 
 -- +goose Down
-DROP TABLE users;
+DROP TABLE exercises;
